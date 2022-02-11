bf46a748a689e62259bf3c6b5e6a398faca485a6
Rashid Goshtasbi <rashid.goshtasbi@gmail.com>
2022-02-10 20:34:00 -0800

    Initial Commit

---
+++ SocketApp/SocketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketAppApp.swift
+//  SocketApp
+//
+//  Created by Rashid Goshtasbi on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SocketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

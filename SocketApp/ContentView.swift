c27e45c797025a5d5cea0ce776eb2852240a293a
Rashid Goshtasbi <rashid.goshtasbi@gmail.com>
2022-02-11 10:15:08 -0800

    working socket chat

--- SocketApp/ContentView.swift
+++ SocketApp/ContentView.swift
@@ -6,13 +6,75 @@
 //
 
 import SwiftUI
+import SocketIO
+import zlib
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+final class Service: ObservableObject {
+    private var manager = SocketManager(socketURL: URL(string: "ws://localhost:3000")!, config: [])
+    
+    //.log(true), .compress
+    
+    @Published var messages = [String]()
+        
+    init() {
+        let socket = manager.defaultSocket
+        socket.on(clientEvent: .connect) { (data, ack) in
+            print("connected")
+            socket.emit("NodeJS Server Port", "Hi Node.JS server!")
+        }
+        
+        socket.on("iOS Client Port") { [weak self] data, ack in
+            if let data = data[0] as?  [String: String],
+               let rawMessage = data["msg"] {
+                DispatchQueue.main.async {
+                    self?.messages.append(rawMessage)
+                    print(rawMessage)
+                }
+            }            
+            socket.emitWithAck("NodeJS Server Port", data)
+        }
+        
+        socket.connect()
     }
 }
+
+struct ContentView: View {
+
+    @ObservedObject var service = Service()
+    
+    init() {
+        UITableView.appearance().tableFooterView = UIView()
+        UITableView.appearance().separatorStyle = .none
+    }
+
+    var body: some View {
+//        ZStack {
+//            List(service.messages.reversed(), id: \.self) { msg in
+//                HStack {
+//                    Text(msg).scaleEffect(x: 1, y: -1, anchor: .center)
+//                }
+//            }.scaleEffect(x: 1, y: -1, anchor: .center)
+//        }
+        
+        ScrollView {
+            VStack(spacing: 20) {
+                ForEach(service.messages.reversed(), id: \.self) { msg in
+                    HStack {
+                        Text("Item \(msg)")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.gray)
+                            .scaleEffect(x: 1, y: -1, anchor: .center)
+                    }
+                }
+            }
+        }.scaleEffect(x: 1, y: -1, anchor: .center)
+//        .frame(height: 350)
+
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
